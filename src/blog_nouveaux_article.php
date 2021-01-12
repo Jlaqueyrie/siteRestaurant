874ba113fc108d185d8a491db150b4e46aa6335b
jlaqueyrie <jerome.laqueyrie@hotmail.com>
2021-01-12 17:09:16 +0100

    connection et ajout d'article Ãa la bdd ok

--- src/blog_nouveaux_article.php
+++ src/blog_nouveaux_article.php
@@ -10,38 +10,55 @@ if(!$conn_bdd){
   //redirection vers page accueil
 }
 //verification champ formulaire
-if($_FILES['photo']['error']){
+if(!empty($_POST['titre']) && !empty($_POST['auteur']) && !empty($_POST['contenu'])){
 
-  switch($_FILES['photo']['error']){
-    case 0:
+  if($_FILES['photo']['error']){
 
-    case 1:
-      printf('taille du fichier supérieur à la limite');
-      break;
-    case 2:
-      printf('taille du fichier trop grande');
-    case 3:
-      printf('envoi du fichier interrompue durant le transfert');
-    case 4:
-      printf('taille du fichier nulle');
+    switch($_FILES['photo']['error']){
+      case 1:
+        printf('taille du fichier supérieur à la limite');
+        break;
+      case 2:
+        printf('taille du fichier trop grande');
+      case 3:
+        printf('envoi du fichier interrompue durant le transfert');
+      case 4:
+        printf('taille du fichier nulle');
 
-  }
-}
-
-if((isset($_FILES['photo']['name'])) && ($_FILES['photo']['error'] == UPLOAD_ERR_OK)){
-  //deplacer l'image du dossier temporaire vers le dossier de destination  
-  $p_dest = dirname(__DIR__).'/www/img/';
-  if(move_uploaded_file($_FILES['photo']['name'], $p_dest.$_FILES['photo']['name'])){
-    //déplacement ok
-    printf('déplacement fichier ok');
+    }
   }
   else{
-    //déplacement nok
-    printf('erreur déplacement fichier photo');
-    var_dump($_FILES);
+
+    if((isset($_FILES['photo']['name'])) && ($_FILES['photo']['error'] == UPLOAD_ERR_OK)){
+      //deplacer l'image du dossier temporaire vers le dossier de destination  
+      $p_dest = dirname(__DIR__).'/www/images/';
+      move_uploaded_file($_FILES['photo']['tmp_name'], $p_dest.$_FILES['photo']['name']);
+      //déplacement ok
+      $sql_insert = "INSERT INTO 
+                    article(titre,auteur,date,image,contenu)
+                    VALUES
+                    ('".htmlentities(addslashes($_POST['titre']),ENT_QUOTES)."',
+                    '".htmlentities(addslashes($_POST['auteur']),ENT_QUOTES)."',
+                    '".date("Y-m-d H:i:s")."',
+                    '".$_FILES['photo']['name']."',
+                    '".htmlentities(addslashes($_POST['contenu']),ENT_QUOTES)."'
+                    )";
+
+      if($sql_send_res = mysqli_query($conn_bdd,$sql_insert)){
+        printf('<div class="alert alert-success text-center">Votre article a bien été mis en ligne</div>');
+      }
+      else{
+        printf('<div class="alert alert-danger text-center">Erreur dans la sauvegarde de l\'article en base de donnée</div>');
+      }
+      mysqli_close($conn_bdd);
+    }
+    else{
+      printf('<div class="alert alert-danger text-center">Le fichier n\'a pas été sauvegardé.</div>');
+    }
   }
-  
 }
+
+  
 $page_contenu = <<<formulaireArticle
 <div class="row">
   <form action="" method="post" class="col-md-6 offset-md-3" enctype="multipart/form-data">
